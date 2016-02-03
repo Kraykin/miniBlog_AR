461fb26d6a411672d39d609d3a55dee121af0428
Kraykin <kraykin@gmail.com>
2016-02-03 12:24:49 +0300

    Add initialisation db

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:minblog.db"
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
